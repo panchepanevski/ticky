0bba9bfab9dc9783a18af2f5b96cec128a9d3fd6
panchepanevski <pance.panevski@gmail.com>
2019-12-17 16:45:25 +0100

    remove unnecessary import from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
 import GlobalStyles from './GlobalStyles';
 import { ThemeProvider } from 'emotion-theming';
 import darkTheme from './themes/darkTheme';
->>>>>>> Stashed changes
-=======
-import GlobalStyles from './GlobalStyles';
-import { ThemeProvider } from 'emotion-theming';
-import darkTheme from './themes/darkTheme';
-import styled from '@emotion/styled';
->>>>>>> 105fd7874aaa89427bb3c080719e5dcd706573a6
 
 function App() {
   return (
