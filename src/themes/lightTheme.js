f5448bf3d0aa0d9c3f783f371eb3091e4fb3edd3
panchepanevski <pance.panevski@gmail.com>
2019-12-17 11:58:12 +0100

    add themes - light and dark

---
+++ src/themes/lightTheme.js
@@ -0,0 +1,15 @@
+const lightTheme = {
+  background: "#EFEFEF";
+  primary: "#191828";
+  secondary: "#22A8B0";
+  ticket_bg: "#222031";
+  status_active: "#7EFFA1";
+  status_in_progres: "#E8E200";
+  status_completed: "#727272";
+  progress: "#21F3FF";
+  priority_normal: "#21AAFF";
+  priority_medium: "#FF9B00";
+  priority_high: "#FF434C";
+}
+
+export default lightTheme;
