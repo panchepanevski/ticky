81fcc162c77c5eb0f48f30947cf15788256d26e7
panchepanevski <pance.panevski@gmail.com>
2019-12-25 14:12:52 +0100

    fix border in input field

--- client/src/components/InputFields/InputAssignedBy.js
+++ client/src/components/InputFields/InputAssignedBy.js
@@ -7,7 +7,7 @@ export const InputAssignedBy = styled.input`
   background-color: transparent;
   box-shadow: 2px 2px 2px #000000;
   border: 1px solid ${props => props.theme.colors.primary};
-  border-radius: 5px 5px 5px 5px;
+  border-radius: 5px;
   color: ${props => props.theme.colors.primary};
   font-size: 16px;
   outline: none;
