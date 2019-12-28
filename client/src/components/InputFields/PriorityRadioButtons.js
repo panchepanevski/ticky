4bb317440cf73ffa2d7e7154bede45517b0bb337
panchepanevski <pance.panevski@gmail.com>
2019-12-28 13:56:56 +0100

    Refactoring and minify existing code

--- client/src/components/InputFields/PriorityRadioButtons.js
+++ client/src/components/InputFields/PriorityRadioButtons.js
@@ -1,17 +1,11 @@
+import React from 'react';
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-around;
-  width: 98%;
+const Input = styled.input`
+  margin: 7px;
 `;
 
-export const Input = styled.input`
-  margin: 5px;
-`;
-
-// PRIORITY NORMAL
-export const NormalLabel = styled.label`
+const Label = styled.label`
   width: 110px;
   display: flex;
   justify-content: center;
@@ -21,54 +15,37 @@ export const NormalLabel = styled.label`
   font-size: 14px;
   text-shadow: 1px 1px 1px #000000;
   background-color: transparent;
-  border: 1px solid ${props => props.theme.colors.priority_normal};
   border-radius: 10px;
+  border: 1px solid
+    ${props => {
+      if (props.field === 'Normal') {
+        return props.theme.colors.priority_normal;
+      } else if (props.field === 'Medium') {
+        return props.theme.colors.priority_medium;
+      } else if (props.field === 'High') {
+        return props.theme.colors.priority_high;
+      } else console.error('This is not a function!!!!');
+    }};
+
   &:hover,
   &:active {
-    background-color: ${props => props.theme.colors.priority_normal_hover};
-    cursor: pointer;
-    transition: 0.3s;
+    background-color: ${props => {
+      if (props.field === 'Normal') {
+        return props.theme.colors.priority_normal_hover;
+      } else if (props.field === 'Medium') {
+        return props.theme.colors.priority_medium_hover;
+      } else if (props.field === 'High') {
+        return props.theme.colors.priority_high_hover;
+      } else console.error('This is not a function!!!!');
+    }};
   }
 `;
 
-// PRIORITY MEDIUM
-export const MediumLabel = styled.label`
-  width: 110px;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  margin: 1px;
-  font-size: 14px;
-  text-shadow: 1px 1px 1px #000000;
-  background-color: transparent;
-  border: 1px solid ${props => props.theme.colors.priority_medium};
-  border-radius: 10px;
-  &:hover,
-  &:focus {
-    background-color: ${props => props.theme.colors.priority_medium_hover};
-    cursor: pointer;
-    transition: 0.3s;
-  }
-`;
-
-// PRIORITY HIGH
-export const HighLabel = styled.label`
-  width: 110px;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  margin: 1px;
-  font-size: 14px;
-  text-shadow: 1px 1px 1px #000000;
-  background-color: transparent;
-  border: 1px solid ${props => props.theme.colors.priority_high};
-  border-radius: 10px;
-  &:hover,
-  &:focus {
-    background-color: ${props => props.theme.colors.priority_high_hover};
-    cursor: pointer;
-    transition: 0.3s;
-  }
-`;
+export default function PriorityRadioButtons({ name, value, field }) {
+  return (
+    <Label field={field}>
+      <Input type="radio" value={value} name={name} />
+      {field}
+    </Label>
+  );
+}
--- client/src/components/InputFields/StatusRadioButtons.js
+++ client/src/components/InputFields/StatusRadioButtons.js
@@ -1,17 +1,11 @@
+import React from 'react';
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-around;
-  width: 98%;
+const Input = styled.input`
+  margin: 7px;
 `;
 
-export const Input = styled.input`
-  margin: 5px;
-`;
-
-// STATUS ACTIVE
-export const ActiveLabel = styled.label`
+const Label = styled.label`
   width: 110px;
   display: flex;
   justify-content: center;
@@ -21,54 +15,37 @@ export const ActiveLabel = styled.label`
   font-size: 14px;
   text-shadow: 1px 1px 1px #000000;
   background-color: transparent;
-  border: 1px solid ${props => props.theme.colors.status_active};
   border-radius: 10px;
+  border: 1px solid
+    ${props => {
+      if (props.field === 'Active') {
+        return props.theme.colors.status_active;
+      } else if (props.field === 'In Progress') {
+        return props.theme.colors.status_in_progres;
+      } else if (props.field === 'Completed') {
+        return props.theme.colors.status_completed;
+      } else console.error('This is not a function!!!!');
+    }};
+
   &:hover,
   &:active {
-    background-color: ${props => props.theme.colors.status_active_hover};
-    cursor: pointer;
-    transition: 0.3s;
+    background-color: ${props => {
+      if (props.field === 'Active') {
+        return props.theme.colors.status_active_hover;
+      } else if (props.field === 'In Progress') {
+        return props.theme.colors.status_in_progres_hover;
+      } else if (props.field === 'Completed') {
+        return props.theme.colors.status_completed_hover;
+      } else console.error('This is not a function!!!!');
+    }};
   }
 `;
 
-// STATUS IN PROGRESS
-export const InProgressLabel = styled.label`
-  width: 110px;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  margin: 1px;
-  font-size: 14px;
-  text-shadow: 1px 1px 1px #000000;
-  background-color: transparent;
-  border: 1px solid ${props => props.theme.colors.status_in_progres};
-  border-radius: 10px;
-  &:hover,
-  &:focus {
-    background-color: ${props => props.theme.colors.status_in_progres_hover};
-    cursor: pointer;
-    transition: 0.3s;
-  }
-`;
-
-// STATUS COMPLETE
-export const CompletedLabel = styled.label`
-  width: 110px;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  margin: 1px;
-  font-size: 14px;
-  text-shadow: 1px 1px 1px #000000;
-  background-color: transparent;
-  border: 1px solid ${props => props.theme.colors.status_completed};
-  border-radius: 10px;
-  &:hover,
-  &:focus {
-    background-color: ${props => props.theme.colors.status_completed_hover};
-    cursor: pointer;
-    transition: 0.3s;
-  }
-`;
+export default function StatusRadioButtons({ name, value, field }) {
+  return (
+    <Label field={field}>
+      <Input type="radio" value={value} name={name} />
+      {field}
+    </Label>
+  );
+}
