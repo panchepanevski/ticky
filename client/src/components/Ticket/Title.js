3e35a4612b530d3c8efa731ae8e9c4212c96bb1d
panchepanevski <pance.panevski@gmail.com>
2019-12-30 00:30:22 +0100

    Add Title Component

--- client/src/components/Ticket/Title.js
+++ client/src/components/Ticket/Title.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const Name = styled.h1`
   color: ${props => props.theme.colors.primary};
-  font-size: 18px;
-  font-weight: 700;
+  font-size: 22px;
+  font-weight: 500;
   text-shadow: 1px 1px 1px #000000;
 `;
