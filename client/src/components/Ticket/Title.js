7b950520848ee233e1ac3e01a6e2d6be625a4188
panchepanevski <pance.panevski@gmail.com>
2020-01-02 16:19:03 +0100

    Refactoring Title component

--- client/src/components/Ticket/Title.js
+++ client/src/components/Ticket/Title.js
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled';
 
-export const Name = styled.h1`
+export const Title = styled.h1`
   color: ${props => props.theme.colors.primary};
   font-size: 22px;
-  font-weight: 500;
+  font-weight: 700;
   text-shadow: 1px 1px 1px #000000;
 `;
