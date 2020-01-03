c39688585eaf06776635e5369b4f3099f4d2e8ee
panchepanevski <pance.panevski@gmail.com>
2020-01-03 15:36:36 +0100

    Add Button Component for Filter Bar

--- client/src/components/Filter Bar/Button.js
+++ client/src/components/Filter Bar/Button.js
@@ -9,11 +9,15 @@ export const Button = styled.button`
   height: 100%;
   background-color: transparent;
   color: ${props => props.theme.colors.primary};
+  outline: none;
+  cursor: pointer;
   font-size: 14px;
   font-weight: 700;
   text-shadow: 1px 1px 1px 1px #000000;
-  border: 1px solid ${props => props.theme.colors.background};
-  &:hover {
+  border: none;
+  &:hover,
+  &:active {
     background-color: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.tertiary};
   }
 `;
