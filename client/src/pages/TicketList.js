0271b447eb08fca83f641ed4b1fae07b8e63410d
panchepanevski <pance.panevski@gmail.com>
2020-01-06 17:15:48 +0100

    Add Header and Filter components and add function

--- client/src/pages/TicketList.js
+++ client/src/pages/TicketList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Ticket from '../components/Ticket/Ticket';
+import Header from '../components/Header/Header';
+import Filter from '../components/Filter Bar/Filter';
 
 const Container = styled.div`
   display: flex;
@@ -10,6 +12,15 @@ const Container = styled.div`
 
 export default function TicketList() {
   const [tickets, setTickets] = React.useState([]);
+
+  const [filter, setFilter] = React.useState('');
+
+  function handleFilterChange(selectedFilter) {
+    setFilter(selectedFilter);
+  }
+
+  const filteredTickets = tickets.filter(ticket => ticket.priority.includes(filter));
+  console.log(filteredTickets);
 
   async function fetchTickets() {
     const response = await fetch('http://localhost:8080/tickets');
@@ -22,21 +33,26 @@ export default function TicketList() {
   }, []);
 
   return (
-    <Container>
-      {tickets.map(ticket => (
-        <Ticket
-          key={ticket.id}
-          id={ticket.id}
-          name={ticket.name}
-          timedate={ticket.timedate}
-          assigned={ticket.assigned}
-          location={ticket.location}
-          status={ticket.status}
-          priority={ticket.priority}
-          details={ticket.details}
-          progress={ticket.progress}
-        />
-      ))}
-    </Container>
+    <>
+      <Header />
+      <Filter onFilterChange={handleFilterChange} />
+
+      <Container>
+        {filteredTickets.map(ticket => (
+          <Ticket
+            key={ticket.id}
+            id={ticket.id}
+            name={ticket.name}
+            timedate={ticket.timedate}
+            assigned={ticket.assigned}
+            location={ticket.location}
+            status={ticket.status}
+            priority={ticket.priority}
+            details={ticket.details}
+            progress={ticket.progress}
+          />
+        ))}
+      </Container>
+    </>
   );
 }
